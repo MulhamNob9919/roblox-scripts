27cd787ada231c338c30c884298c3f3c74bcb1a5
MulhamNob9919 <77995622+MulhamNob9919@users.noreply.github.com>
2022-10-10 17:19:51 +0300

    Update simplespy.lua

--- simplespy.lua
+++ simplespy.lua
@@ -1,1 +1,1 @@
-loadstring(game:GetObjects("https://raw.githubusercontent.com/exxtremestuffs/SimpleSpySource/master/SimpleSpy.lua")[1].Source)()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/exxtremestuffs/SimpleSpySource/master/SimpleSpy.lua"))();
